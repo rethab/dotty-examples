97823710a609012da146abf3a367bac8ab5db201
Reto Habluetzel <rethab@protonmail.ch>
2016-11-07 23:16:01 +0100

    comparison with scala 'with'

--- IntersectionTypes.scala
+++ IntersectionTypes.scala
@@ -5,6 +5,11 @@ case class Person(name: String) extends Loud with Mutable
 
 object Hello {
 
+  /* kind of possible in scala with 'with', but
+   * 'A with B' was not the same as 'B with A'.
+   * '&' is commutative in dotty, meaning
+   * 'A & B' is the same as 'B & A'
+   */
   def sayHello(thing: Loud & Mutable): String =
     thing.hello + " " + thing.becomeQuiet
 
